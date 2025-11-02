02177021656b604edc640834fca9d0202b1411a9
박수휘 <tngnl12341@gmail.com>
2025-11-02 19:54:10 +0900

    레이아웃) App, main tsx 파일 정리

--- portfolio-movie-app/src/main.tsx
+++ portfolio-movie-app/src/main.tsx
@@ -1,7 +1,10 @@
+/**
+ * main.tsx
+**/
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import App from './App'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
