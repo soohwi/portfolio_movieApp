2bf74222ef89eae831c315eb33397b6fd6fa755e
박수휘 <tngnl12341@gmail.com>
2025-11-02 21:06:40 +0900

    build) 깃 빌드 수정중

--- portfolio-movie-app/vite.config.ts
+++ portfolio-movie-app/vite.config.ts
@@ -1,7 +1,11 @@
+/**
+ * vite.config.ts
+**/
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
+  base: '/portfolio_movieApp/',
   plugins: [react()],
-})
+});
