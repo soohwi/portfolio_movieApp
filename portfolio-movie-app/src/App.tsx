a8ab201372053676159aa2a58ba9db7c4098ad6f
박수휘 <tngnl12341@gmail.com>
2025-11-02 21:06:23 +0900

    공통) GlobalStyle 참조 위치 main으로 변경

--- portfolio-movie-app/src/App.tsx
+++ portfolio-movie-app/src/App.tsx
@@ -4,20 +4,16 @@
 
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { routes } from "./routes/routes";
-import GlobalStyle from "./styles/GlobalStyle";
 
 function App() {
   return (
-    <>
-      <GlobalStyle />
-      <Router>
-        <Routes>
-          {routes.map((route) => (
-            <Route key={route.path} path={route.path} element={route.element} />
-          ))}
-        </Routes>
-      </Router>
-    </>
+    <Router>
+      <Routes>
+        {routes.map((route) => (
+          <Route key={route.path} path={route.path} element={route.element} />
+        ))}
+      </Routes>
+    </Router>
   )
 }
 
--- portfolio-movie-app/src/main.tsx
+++ portfolio-movie-app/src/main.tsx
@@ -2,12 +2,17 @@
  * main.tsx
 **/
 
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
+import App from './App';
+import GlobalStyle from "./styles/GlobalStyle";
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <BrowserRouter basename="/portfolio_movieApp/">
+      <GlobalStyle />
+      <App />
+    </BrowserRouter>
   </StrictMode>,
 )
